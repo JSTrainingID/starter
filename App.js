9414f77e36502fb513ccc3b9006c31f4548ef462
broerjuang <juangwiantoro@gmail.com>
2017-04-18 14:27:14 +0700

    Add Lint

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 // @flow
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 export default class App extends React.Component {
   render() {
